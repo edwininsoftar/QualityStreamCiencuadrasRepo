f8ba907ed3b7ccbf9d985c171a33b3766c5d0ed2
edwininsoftar <edwin.pulido@insoftar.com>
2022-03-14 10:49:44 -0500

    create page

--- src/test/java/createPropertyStream/CreatePropertyPage.java
+++ src/test/java/createPropertyStream/CreatePropertyPage.java
@@ -1,4 +1,4 @@
-package postStream;
+package createPropertyStream;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,4 +15,16 @@ public class CreatePropertyPage extends BasicWrap {
 	By locator_into = By.xpath("//*[@id=\"headerHome\"]/lib-cc-header/header/nav[2]/button[1]/a");
 	By locator_username = By.cssSelector("input[formcontrolname=\"username\"]");
 	By locator_password = By.cssSelector("input[formcontrolname=\"password\"]");
+	By locator_login = By.cssSelector("span[class=\"ng-star-inserted\"]");
+	
+	String username = "juanlag4545@yopmail.com";
+	String password = "Ciencuadras21*";
+	
+	public void login() throws InterruptedException {
+		Thread.sleep(6000);
+		click(locator_into);
+		type(username, locator_into);
+		type(password, locator_password);
+		click(locator_login);
+	}
 }
