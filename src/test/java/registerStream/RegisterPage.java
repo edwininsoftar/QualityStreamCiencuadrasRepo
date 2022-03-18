93662453cb4fb516250c98d7572dcc4fe5a610d3
edwininsoftar <edwin.pulido@insoftar.com>
2022-03-18 11:06:33 -0500

    adjustment

--- src/test/java/registerStream/RegisterPage.java
+++ src/test/java/registerStream/RegisterPage.java
@@ -1,13 +1,8 @@
 package registerStream;
 
-import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import Base.BasicWrap;
 import io.netty.handler.timeout.TimeoutException;
 
