a0916bf0357b19f046d09fa6a003a68e93cc1327
edwininsoftar <edwin.pulido@insoftar.com>
2022-03-30 16:13:52 -0500

    herramientas y servicios
    Ajuste herramientas y servicios

--- src/test/java/serviceToolsStream/RequestAppraisalPage.java
+++ src/test/java/serviceToolsStream/RequestAppraisalPage.java
@@ -16,7 +16,7 @@ public class RequestAppraisalPage extends BasicWrap {
 	By locator_buttonColombia = By.xpath("/html/body/app-root/app-appraisal/section[3]/app-card-option/div/div/div[1]/a");
 	By locator_buttonExterior = By.xpath("/html/body/app-root/app-appraisal/section[3]/app-card-option/div/div/div[2]/a");
 	By locator_buttonCalculatePrice = By.xpath("/html/body/app-root/app-home/app-cards-services/section/owl-carousel-o/div/div[1]/owl-stage/div/div/div[1]/div/div[2]/div/div[2]/a");
-	By locator_city = By.name("ciudad");
+	By locator_city = By.name("ciudad");  
 	By locator_confirm = By.xpath("//*[@id=\"cdk-step-content-0-0\"]/app-check-coverage/div/div[2]/div[2]/button");
 	By locator_continue = By.xpath("//*[@id=\"mat-dialog-0\"]/app-confirm-coverage/div/div[3]/div/button");
 	// Formato de solicitud
@@ -61,6 +61,21 @@ public class RequestAppraisalPage extends BasicWrap {
 	By locator_ownerEmail = By.id("mat-input-18");
 	By locator_processingPersonalData = By.xpath("//*[@id=\"mat-checkbox-2\"]/label/span[1]");
 	By locator_pay = By.xpath("//*[@id=\"cdk-step-content-0-2\"]/app-user-info/div/div/form/div[4]/button[2]");
+	//Pago y descuento ciencuadras 
+	By locator_bond = By.cssSelector("input[formcontrolname=\"discount\"]");
+	By locator_aplic = By.xpath("/html/body/app-root/app-products-checkout/car-summary/div[1]/section/div/div[2]/div[2]/app-coupon/div/form/div/button/span");
+	By locator_buttonPayfinish = By.xpath("//*[@id=\"payment-data\"]/div/div/button");
+	//Datos de pago cliente
+	By locator_cookies = By.xpath("/html/body/app-root/app-payment/div[2]/div/button");
+	By locator_pse = By.xpath("//*[@id=\"mat-radio-4\"]/label/div[1]/div[1]"); 
+	By locator_paymentP = By.xpath("//*[@id=\"pay-pse\"]/form/div[2]/button");
+	By locator_typedocument = By.name("documentType");
+	By locator_TypePerson = By.name("personType");
+	By locator_bank = By.name("bank");
+	By locator_debitCard = By.xpath("//*[@id=\"mat-radio-2\"]/label/div[1]/div[1]");
+	By locator_paymentD = By.xpath("//*[@id=\"formulario_card-debit\"]/div[2]/button");
+	By locator_creditCard = By.xpath("//*[@id=\"mat-radio-2\"]/label/div[1]/div[2]");
+	By locator_paymentC = By.xpath("//*[@id=\"formulario_card-credit\"]/div[2]/button");
 	
 	String appraisalFrom = "Colombia";//Colombia, Exterior
 	String city = "Bogotá"; 
@@ -91,6 +106,13 @@ public class RequestAppraisalPage extends BasicWrap {
 	String ownerPhone = "204436343"; // Ingrese el numero del propietario sin el 3
 	String ownerEmail = "pruebacorreopropietario@yopmail.com";// Ingrese el correo del propietario 
 	String processingPersonalData = "No";//Ingrese SI o No si acepta el tratamiento de datos personales
+	//Pago y descuento ciencuadras
+	String discountCode = "12354786";
+	//Dastos cliente pago
+	String paymentType = "Debito";// Debito, Credito, PSE
+	String typeDocument = "Número de identificación tributario";// Cédula de Ciudadania, Cédula de Extranjeria, Cédula de Pasaporte, Número de identificación tributario, Tarjeta de identidad
+	String typePerson = "Persona natural";//Persona natural, Persona Juridica
+	String typeBank = "BANCO DAVIVIENDA";//BANCO DAVIVIENDA, BANCO CACJA SOCIAL, BANCON COLPATRIA, BANCOLOMBIA
 
 	public RequestAppraisalPage(WebDriver driver) {
 		super(driver);
@@ -229,6 +251,48 @@ public class RequestAppraisalPage extends BasicWrap {
 				click(locator_processingPersonalData);
 			}
 			click(locator_pay);
+		} catch (Exception e) {
+			System.out.println("Error: "+e);
+		}
+	}
+	
+	public void payFinish() {
+		try {
+			Thread.sleep(20000);
+			if(discountCode != "") {
+				type(discountCode, locator_bond);
+				click(locator_aplic);
+			}
+			click(locator_buttonPayfinish);
+		} catch (Exception e) {
+			System.out.println("Error: "+e);
+		}
+	}
+	
+	public void purchaseDetail() {
+		try {
+			click(locator_cookies);
+			if(paymentType.equals("Debito")) {
+				click(locator_debitCard);
+				click(locator_paymentD);
+			}
+			if(paymentType.equals("Credito")) {
+				click(locator_creditCard);
+				click(locator_paymentC);
+			}
+			if(paymentType.equals("PSE")) {
+				click(locator_pse);
+				WebElement tc = driver.findElement(locator_typedocument);
+				Select sel = new Select(tc);
+				sel.selectByVisibleText(typeDocument);
+				WebElement tp = driver.findElement(locator_TypePerson);
+				Select selp = new Select(tp);
+				selp.selectByVisibleText(typePerson);
+				WebElement tb = driver.findElement(locator_bank);
+				Select selb = new Select(tb);
+				selb.selectByVisibleText(typeBank);
+				click(locator_paymentP);
+			}
 		} catch (Exception e) {
 			System.out.println("Error: "+e);
 		}
