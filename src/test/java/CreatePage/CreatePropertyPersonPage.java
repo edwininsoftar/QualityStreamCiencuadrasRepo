cfcc6721a750c8901f3685b122f4fa6ca8ebd4b4
edwininsoftar <edwin.pulido@insoftar.com>
2022-04-18 10:24:01 -0500

    flujo crear persona
    creación de flujo crear rol persona

---
+++ src/test/java/CreatePage/CreatePropertyPersonPage.java
@@ -0,0 +1,14 @@
+package CreatePage;
+
+import org.openqa.selenium.WebDriver;
+
+import Base.BasicWrap;
+
+public class CreatePropertyPersonPage extends BasicWrap{
+
+	public CreatePropertyPersonPage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+}
