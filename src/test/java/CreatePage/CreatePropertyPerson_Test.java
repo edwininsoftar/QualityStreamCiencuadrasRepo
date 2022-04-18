d252f9a60a83b96a0974e17476083f9cf76c5ece
edwininsoftar <edwin.pulido@insoftar.com>
2022-04-18 16:47:00 -0500

    Flujo crear rol persona

--- src/test/java/CreatePage/CreatePropertyPerson_Test.java
+++ src/test/java/CreatePage/CreatePropertyPerson_Test.java
@@ -1,24 +1,32 @@
 package CreatePage;
 
-import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
 
 public class CreatePropertyPerson_Test {
+	
+	private WebDriver driver;
+	CreatePropertyPersonPage createPropertyPersonPage;
 
 	@Before
 	public void setUp() throws Exception {
+		createPropertyPersonPage = new CreatePropertyPersonPage(driver);
+		driver = createPropertyPersonPage.chromeDriverConnection();
+		createPropertyPersonPage.visit("https://qa.ciencuadras.com/");
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		//driver.quit();
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		createPropertyPersonPage.login();
+		createPropertyPersonPage.published();   
+		createPropertyPersonPage.purchaseForm();
 	}
 
 }
